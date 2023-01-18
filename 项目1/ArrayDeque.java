dbead3738bbbd2add7ad7cb0b60a985dc086081c
vvwind <98531858+javierheng@users.noreply.github.com>
2023-01-18 15:06:32 +0800

    Update ArrayDeque.java

--- 项目1/ArrayDeque.java
+++ 项目1/ArrayDeque.java
@@ -1,87 +1,104 @@
-public class ArrayDeque<type>{
-    //属性
-    type[] array;
-    type[] tmp;
-    int realsize = 2;
-    int sentinel2 = 1;
+//import javax.rmi.*;
 
+public class ArrayDeque<T>{
+    //属性
+    T[] array;
+    //T[] tmp;
+    int size = 0;
+    int length ;
+    //int front = 0;
+    int last;
+
+    //任务拆解:数组扩容、空出位置1
+    public void expand(){
+        T[] tmp = (T[]) new Object[length];
+        System.arraycopy(array, 0, tmp, 0, length);
+        array =(T[]) new Object[length * 2];
+        length = length * 2;
+        System.arraycopy(tmp,0,array,0,length/2);
+    }
+    
+    public T[] getfirstspace(T[] a){
+        T[] tmp =(T[]) Object(length);
+        System.arraycopy(a, 0, tmp, 1, size);
+        return tmp;
+    }
+    
 	//构造
-    public ArrayDeque(type x){
-        size += 1;
-        array=new type[]{null,x,null};
-        sentinel2 += 1;
+    public ArrayDeque(T x){
+        array=(T[]) Object(100);
+        array[0] = x;
+        this.last = 0;
+        size = 1;
+        length = 100;
     }
 
     public ArrayDeque(){
-        array=new type[]{null,null};
+        array=(T[]) Object(100);
+        size = 0;
+        length = 100;
+        last = -1;
     }
 
     public ArrayDeque(ArrayDeque a){
-        this.realsize = a.realsize;
-        array = new type[realsize];
-        System.arraycopy(a, 0, array, 0, a.realsize);
+        this.size = a.size;
+        this.length = a.length;
+        this.last = a.last;
+        array=(T[]) Object(length);
+        System.arraycopy(a, 0, array, 0, size);
+        
     }
     
     //Deque方法的实现
-    public void addFirst(type x){
-        tmp = new type[realsize];
-        System.arraycopy(arrar, 0, tmp, 0, realsize);
-        realsize += 1;
-        array = new type[realsize];
-        array[0] = null;
-        array[1] = x;
-        System.arraycopy(tmp, 1, array, 2, realsize - 2);
-        sentinel2 = realsize - 1;
+    public void addFirst(T x){
+        if(size == length) expand();
+        array = getfirstspace(array);
+        array[0] = x;
+        size = size + 1;
+        last = last + 1;
     }
     
-    public void addLast(type x){
-        tmp = new type[realsize];
-        System.arraycopy(arrar, 0, tmp, 0, realsize);
-        realsize += 1;
-        array = new type[realsize];
-        System.arraycopy(tmp, 0, array, 0, realsize - 2);
-        array[realsize - 2] = x;
-        array[realsize - 1] = null;
-        sentinel2 = realsize - 1;
+    public void addLast(T x){
+        if(size == length) expand();
+        array[last + 1]=x;
+        last += 1;
+        size += 1;
     }
     
     public boolean isEmpty(){
-        if(realsize == 2) return true;
+        if(this.size == 0) return true;
         else return false;
     }
     
     public int size(){
-        return realsize - 2;
+        return this.size;
     }
     
     public void printDeque(){
-        int num = 1;
-        while(num != sentinel2-1){
+        int num = 0;
+        while(num != size){
             System.println(array[num]);
             num = num + 1;
         }
     }
     
     public void removeFirst(){
-        tmp = new type[realsize];
-        System.arraycopy(array, 0, tmp, 0, realsize);
-        realsize -= 1;
-        array = new type[realsize];
-        array[0] = null;
-        System.arraycopy(tmp, 2, array, 1, realsize - 1);
+        T[] tmp =(T[]) new Object[length];
+        System.arraycopy(array, 1, tmp, 0, size-1);
+        array = tmp;
+        size -= 1;
+        last -= 1;
     }
     
     public void removeLast(){
-        tmp = new type[realsize];
-        System.arraycopy(array, 0, tmp, 0, realsize);
-        realsize -= 1;
-        array = new type[realsize];
-        System.arraycopy(tmp, 0, array, 0, realsize-1);
-        array[realsize] = null;
+        array[last] = null;
+        last -= 1;
+        size -= 1;
     }
     
-    public type get(int num){
-        return array[num];
+    public T get(int num){
+        if((num<0)||(num>last)) return null;
+        else return array[num];
     }
 }
 
